bb18901ec49e892f5e07990c5df5c425e0d1ce37
Renato Bertizini <rbertizini@gmail.com>
2022-10-06 10:20:46 -0300

    Aula dia 06/10

--- webapp/controller/Binding.controller.js
+++ webapp/controller/Binding.controller.js
@@ -14,9 +14,24 @@ sap.ui.define([
                 var oModelo = new JSONModel("json/modelo.json");
                 this.getView().setModel(oModelo, "DataMod");
 
-                var oCategoria = new JSONModel("json/categoria.json");
+                var oCategoria = new JSONModel("");
                 this.getView().setModel(oCategoria, "DataCat");
 
-            }
+            },
+
+            cboxCategFill: function(joao) {
+                //alert(joao.getParameter("selectedItem").getKey());
+
+                var oCategoria = new JSONModel("json/categoria.json");
+                this.getView().setModel(oCategoria, "DataCat");
+                this.getView().byId("cboxCateg").getBinding("items").filter([
+                    new sap.ui.model.Filter(
+                        "idmodelo",
+                        "EQ",
+                        joao.getParameter("selectedItem").getKey())
+                ]);
+
+
+            },
         });
     });
